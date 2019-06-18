7d665d4e74c718c29dc8079e9e51cdc3b154642a
Ashal aka JOKER <ahastudio@gmail.com>
2019-06-19 05:32:05 +0900

    Use Express response
    Use a Response when the Express application receives a request.
    
    See https://expressjs.com/en/api.html#res
    
    Write a README file.
    
    Lint and fix JavaScript source code files using ESLint.

---
+++ line-bot/README.md
@@ -0,0 +1,21 @@
+# Line Messenger Bot
+
+## Install dependencies
+
+```bash
+npm ci
+```
+
+## Run server
+
+```bash
+npm start
+```
+
+## Test with `curl`
+
+```bash
+curl http://localhost:3000/
+
+curl -X POST http://localhost:3000/webhook
+```
--- line-bot/app.js
+++ line-bot/app.js
@@ -1,26 +1,31 @@
-//jshint esversion:8
+// jshint esversion:8
 
-const express = require("express");
+const express = require('express');
 // const request = require("request");
 // const ngrok = require("ngrok");
 
+const HOST = '0.0.0.0';
+const PORT = process.env.POST || '3000';
+
+const { log } = console;
+
 const app = express();
 
-let hostName = [0,0,0,0];
-// let port = normalizePort(process.env.POST || '3000');
-
-app.get("/", function(req, res){
-  res.send("Hello World");
+app.get('/', (_req, res) => {
+  log('GET /');
+  res.send('Hello World\n');
 });
 
-app.post("/webhook", function(req, res){
+app.post('/webhook', (_req, res) => {
+  log('POST /webhook');
+  res.send('Hi!\n');
 
   // let options = {
   //   url: "https://api.line.me/v2/bot/message/push",
-    // method: "POST",
-    // header: {
-      // "bearer" :
-    // };
+  // method: "POST",
+  // header: {
+  // "bearer" :
+  // };
   // };
 
   // request(options, function(error, response, body){
@@ -29,6 +34,6 @@ app.post("/webhook", function(req, res){
 });
 
 
-app.listen(process.env.POST || '3000', hostName, function(req, res){
-  console.log("Server is running on port 3000.");
+app.listen(PORT, HOST, () => {
+  log('Server is running on port 3000.');
 });
